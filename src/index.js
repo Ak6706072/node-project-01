562230e51743d9aa3e201d11a135e1eb85ef3422
Ak6706072 <42826806+Ak6706072@users.noreply.github.com>
2021-05-10 21:31:26 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 //app config
 const app = express();
 
 //middleware
-app.use(bodyParser.json());
 
-//database
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  next();
+  res.send("New Hello 1234 World");
+});
+app.get("/", (req, res, next) => {
+  res.send("hello");
+});
+app.post("/post-anything", (req, res) => {
+  const { name, role } = req.body;
+  console.log({ name, role });
+  res.status(200).json(`The entered data is ${name} and ${role}`);
+});
+
+//databas
 const url = `mongodb+srv://ak6706072:ux9acavqz2@cluster0.pcdlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose
   .connect(url, {
